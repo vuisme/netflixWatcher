5073d7d7df30e355fcface1c3f09868f793863ba
cpanel10x <me@vutn.net>
2024-06-03 15:21:25 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import logging
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
 # Cấu hình logging
 logging.basicConfig(level=logging.INFO)
