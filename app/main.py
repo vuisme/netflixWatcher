bc33128f017f6f79adf75eeb3b168c23347da7c9
cpanel10x <me@vutn.net>
2024-06-05 17:20:04 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 import logging
 from googleapiclient.discovery import build
 from googleapiclient.discovery_cache.base import Cache
